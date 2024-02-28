bd4df493816c5167fbf2ac1c590910f30cefe6d5
sperlingxx <lovedreamf@gmail.com>
2024-02-28 18:08:55 +0900

    the PoC impl of hybrid parquet reader

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
@@ -1498,6 +1498,13 @@ val GPU_COREDUMP_PIPE_PATTERN = conf("spark.rapids.gpu.coreDump.pipePattern")
     .booleanConf
     .createWithDefault(false)
 
+  val PARQUET_READ_ON_HOST = conf("spark.rapids.sql.parquet.readOnHost")
+    .doc("Leverages CPUs to read parquet files if there is no available GPU. " +
+      "The host-side reader produces cuDF column vector directly. R2C is unnecessary.")
+    .internal()
+    .integerConf
+    .createWithDefault(-1)
+
   val ORC_DEBUG_DUMP_PREFIX = conf("spark.rapids.sql.orc.debug.dumpPrefix")
     .doc("A path prefix where ORC split file data is dumped for debugging.")
     .internal()
@@ -2499,6 +2506,8 @@ class RapidsConf(conf: Map[String, String]) extends Logging {
   lazy val parquetDebugDumpPrefix: Option[String] = get(PARQUET_DEBUG_DUMP_PREFIX)
 
   lazy val parquetDebugDumpAlways: Boolean = get(PARQUET_DEBUG_DUMP_ALWAYS)
+
+  lazy val parquetReadOnHost: Int = get(PARQUET_READ_ON_HOST)
 
   lazy val orcDebugDumpPrefix: Option[String] = get(ORC_DEBUG_DUMP_PREFIX)
 
